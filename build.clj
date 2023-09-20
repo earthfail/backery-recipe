1d98d08edca6870799e1b9630de5f9c517ca6ec6
earthfail <saleemkhatib22@gmail.com>
2023-09-21 01:15:59 +0300

    improve build.clj

--- build.clj
+++ build.clj
@@ -3,6 +3,8 @@
    [clojure.tools.build.api :as b]
    [asset-minifier.core :as am]
    [selmer.parser :as selmer]
+   ;; [selmer.util :as sl]
+   [clojure.edn :as edn]
    [clojure.java.io :as io]))
 
 (def build-folder "target")
@@ -26,17 +28,27 @@
     (println "compile " (.getPath css-file))
     (b/process {:command-args ["npx" "tailwindcss" "--input" (.getPath css-file) "--output" (str "public/css/" (.getName css-file))]
                 :env {"PROJ_ENV" "production"}})))
-(defn compile-cljs [{builds :builds :or {builds ["recipe" "register" "make"]}}]
-  (b/process {:command-args (into
-                             ["npx" "shadow-cljs" "release"]
-                             (map str builds))}))
+(defn compile-cljs [{builds :builds}]
+  (let [builds (if builds builds
+                   (with-open [f (io/reader "shadow-cljs.edn")]
+                     (-> f
+                         (java.io.PushbackReader.)
+                         edn/read
+                         (get :builds)
+                         keys)))]
+    (println "builds are" builds)
+    (b/process {:command-args (into
+                               ["npx" "shadow-cljs" "release"]
+                               (map str builds))})))
 (defn compile-html
   "Given a `context-map` for `selmer.parser/render-file`,takes html files in resources/pages
   and output them to public folder"
   [context-map]
+  ;; (println "selmer custom path is " sl/*custom-resource-path*)
   (doseq [html-file (.list (io/file (io/resource "pages")))]
+    (println "compile " html-file)
     (spit (str "public/" html-file)
-          (selmer/render-file (str "resources/pages/" html-file) context-map))))
+          (selmer/render-file (str "pages/" html-file) context-map))))
 (defn minify-html [_]
   (println "minify html templates...")
   (am/minify-html "resources/templates" "minified/templates" {:enabled true :remove-comments true :remove-multi-spaces true :compress-css true})
@@ -50,53 +62,47 @@
   (println "minify css...")
   (compile-css nil)
   )
-(defn uber [{:keys [css html js], :or {css true html true js true}}]
+(defn uber [{:keys [css html template cljs clean?], :or {css false html false template false cljs false clean? true}}]
   (clean nil)
   ; (println "compile cljs to js...")
   ; (compile-cljs nil)
   ; (b/delete {:path "public/js/cljs-runtime"})
   ; (b/delete {:path "public/js/manifest.edn"})
+  (b/copy-dir {:src-dirs ["resources"]
+               :target-dir "tmp/resources"})
+
   (when css
     (compile-css nil))
-  (when js
+  (when cljs
     (compile-cljs nil))
 
-  (compile-html {})
-  (if html
+  (when template
+    (compile-html {}))
+  (when html
     (do
       (minify-html nil)
       (b/copy-dir {:src-dirs ["minified/templates"]
                    :target-dir "tmp/resources/templates"})
       (b/copy-dir {:src-dirs ["minified/pages"]
-                   :target-dir "tmp/public"}))
-    ;else
-    (do
-      (b/copy-dir {:src-dirs ["resources/templates"]
-                   :target-dir "tmp/resources/templates"})
-      (b/copy-dir {:src-dirs ["public"]
-                   :target-dir "tmp/public"})))
- 
+                   :target-dir "public"})))
   
-
-  (b/copy-dir {:src-dirs ["resources"]
-               :target-dir "tmp/resources"})
-  ;; (println "minify html+css")
-  ;; (minify nil)
   (b/copy-dir {:src-dirs ["tmp/resources"]
                :target-dir jar-content})
-  (b/copy-dir {:src-dirs ["tmp/public"]
-               :target-dir (str jar-content "/public")})
-  
-  (b/delete {:path "tmp"})
-  
-  ;; (b/compile-clj {:basis basis
-  ;;                 :src-dirs ["src"]
-  ;;                 :class-dir jar-content})
-  ;; (b/uber {:class-dir jar-content
-  ;;          :uber-file uber-file-name
-  ;;          :basis basis
-  ;;          ;; :main 'khsalim.backend.server
-  ;;          :main 'khsalim.backend.core
-  ;;          })
+
+  (when clean?
+    (println "deleting tmp and minified")
+    (b/delete {:path "tmp"})
+    (b/delete {:path "minified"}))
+
+  (println "compile clj...")
+  (b/compile-clj {:basis basis
+                  :src-dirs ["src"]
+                  :class-dir jar-content})
+  (b/uber {:class-dir jar-content
+           :uber-file uber-file-name
+           :basis basis
+           ;; :main 'khsalim.backend.server
+           :main 'khsalim.backend.core
+           })
   (println (format "Uber file created \"%s\"" uber-file-name)))
 
